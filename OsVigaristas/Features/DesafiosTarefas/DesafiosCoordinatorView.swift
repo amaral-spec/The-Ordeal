33dd6bcaea889183b62986daf93a3d95dfff4814
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-11-10 11:15:49 -0300

    desafios e tarefas 0.1

--- OsVigaristas/Features/DesafiosTarefas/DesafiosCoordinatorView.swift
+++ OsVigaristas/Features/DesafiosTarefas/DesafiosCoordinatorView.swift
@@ -1,25 +1,164 @@
 //
-//  DesafiosCoordinatorView.swift
 //  OsVigaristas
 //
-//  Created by Ludivik de Paula on 04/11/25.
+//  Created by Erika Hacimoto on 04/11/25.
 //
 
 import SwiftUI
 
-struct DesafiosCoordinatorView: View {
-    enum Route: Hashable {
-        case list
-        case detalhe(Desafio)
-    }
-
-    @State private var path = [Route]()
-    let isProfessor: Bool
-
+struct Card: View {
+    let index: Int
+    
     var body: some View {
-        
-        Text("Desafio")
-        
+        ZStack {
+            Image("violao")
+                .resizable()
+        }
+        .frame(width: 370, height: 190)
+        .cornerRadius(30)
+    }
+}
+
+struct HomeView: View {
+    @State private var numChallenge: Int = 0
+    @State private var numTask: Int = 0
+    enum Mode: String, CaseIterable {
+        case Desafio, Tarefa
+    }
+    @State private var selectedMode = Mode.Desafio
+    var body: some View {
+        NavigationStack {
+            Picker("", selection: $selectedMode) {
+                ForEach(Mode.allCases, id: \.self) { mode in Text(mode.rawValue) }
+            }
+            .pickerStyle(.segmented)
+            .frame(width: 350, height: 30, alignment: .center)
+            VStack (spacing: -15){
+                if selectedMode == Mode.Desafio {
+                    if numChallenge == 0 {
+                        Spacer(minLength: 100)
+                        ZStack {
+                            Image(systemName: "flag.pattern.checkered.2.crossed")
+                                .foregroundColor(Color.accentColor)
+                            Circle()
+                                .fill(Color.accentColor)
+                                .opacity(0.3)
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .padding()
+                                                }
+                            VStack (spacing: -15){
+                            Text("Sem Desafios")
+                                .font(.title3)
+                                .foregroundColor(.primary)
+                                .fontWeight(.bold)
+                                .padding()
+                            Text("Você não possui nenhum desafio")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                                .fontWeight(.medium)
+                        }
+                        Spacer()
+                        
+                        Button {
+                            numChallenge += 1
+                        } label: {
+                            Text("Novo Desafio")
+                                .frame(maxWidth: 320, maxHeight: 30)
+                        }
+                        .frame(minWidth: 350, minHeight: 70)
+                        .padding(.horizontal)
+                        .buttonStyle(.glassProminent)
+                    } else {
+                        ScrollView{
+                            VStack {
+                                Spacer()
+                                ForEach(0..<numChallenge, id: \.self) {
+                                    i in Card(index: i)
+                                    Spacer(minLength: 20)
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if numTask == 0 {
+                        Spacer(minLength: 100)
+                        ZStack {
+                            Image(systemName: "checklist.checked")
+                                .foregroundColor(Color.accentColor)
+                            Circle()
+                                .fill(Color.accentColor)
+                                .opacity(0.3)
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .padding()
+                        }
+                        
+                        Text("Sem Tarefas")
+                            .font(.title3)
+                            .foregroundColor(.primary)
+                            .fontWeight(.bold)
+                            .padding()
+                        Text("Você não possui nenhuma tarefa")
+                            .font(.headline)
+                            .foregroundColor(.secondary)
+                            .fontWeight(.medium)
+                        
+                        Spacer()
+                        
+                        Button {
+                            numTask += 1
+                        } label: {
+                            Text("Nova Tarefa")
+                                .frame(maxWidth: 320, maxHeight: 30)
+                        }
+                        .frame(maxWidth: 350, minHeight: 70)
+                        .padding(.horizontal)
+                        .buttonStyle(.glassProminent)
+                    } else {
+                        ScrollView{
+                            VStack {
+                                Spacer()
+                                ForEach(0..<numTask, id: \.self) {
+                                    i in Card(index: i)
+                                    Spacer(minLength: 20)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resumo")
+            .toolbarTitleDisplayMode(.inlineLarge)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Adicionar", systemImage: "plus") {
+                        if selectedMode == Mode.Desafio {
+                            numChallenge += 1
+                        } else {
+                            numTask += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
+//struct DesafiosCoordinatorView: View {
+//    enum Route: Hashable {
+//        case list
+//        case detalhe(Desafio)
+//    }
+//
+//    @State private var path = [Route]()
+//    let isProfessor: Bool
+//
+//    var body: some View {
+//        
+//        Text("Desafio")
+//        
 //        NavigationStack(path: $path) {
 //            DesafiosListView(viewModel: makeViewModel())
 //                .navigationDestination(for: Route.self) { route in
@@ -31,11 +170,11 @@ struct DesafiosCoordinatorView: View {
 //                    }
 //                }
 //        }
-    }
-
+//    }
+//
 //    private func makeViewModel() -> DesafiosListViewModel {
 //        userType == .professor
 //            ? ProfessorDesafiosViewModel()
 //            : AlunoDesafiosViewModel()
 //    }
-}
+//}
