a9811fd0836bef7317cf9590bf6527f857190681
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-11-24 10:12:45 -0300

    CriarTarefaView

--- OsVigaristas/Features/Resume/professor/CriarTarefaView.swift
+++ OsVigaristas/Features/Resume/professor/CriarTarefaView.swift
@@ -9,14 +9,12 @@
 import SwiftUI
 import CloudKit
 
-@MainActor
-private func grupos() -> [GroupModel] {
-    return [
-        GroupModel(name: "JJ", image: nil),
-        GroupModel(name: "ALIEN", image: nil),
-        GroupModel(name: "Maria Maria", image: nil)
-    ]
-}
+//@MainActor
+private let groups = [
+    GroupModel(name: "JJ", image: nil),
+    GroupModel(name: "ALIEN", image: nil),
+    GroupModel(name: "Maria Maria", image: nil)
+]
 
 struct CriarTarefaView: View {
     @State private var selectedItem: Int = 0
@@ -30,8 +28,7 @@ struct CriarTarefaView: View {
     @EnvironmentObject var persistenceServices: PersistenceServices
     @State private var isSaving = false
     var onTarefaCriada: (() -> Void)?
-    @State private var participants: [UserModel] = []
-    @State private var selectedUserID: CKRecord.ID? = nil
+    @State private var selecao: CKRecord.ID? = nil
         
     init(numTask: Binding<Int>) {
         self._numTask = numTask
@@ -47,14 +44,14 @@ struct CriarTarefaView: View {
     private var participantesSection: some View {
         Section {
             LabeledContent("Participantes") {
-                NavigationLink(destination: AddGrupo(selectedUserID: $selectedUserID, grupos: grupos())) {
+                NavigationLink(destination: List(groups, selection: $selecao) { group in Text(group.name) } ) {
                     HStack{
                         Spacer()
-                        if selectedUserID == nil {
+                        if selecao == nil {
                             Text("Nenhum")
                         } else {
-                            if let grupoSelecionado = grupos().first(where: { $0.id == selectedUserID }) {
-                                Text(grupoSelecionado.name)
+                            if let selectedGroup = groups.first(where: { $0.id == selecao } ) {
+                                Text(selectedGroup.name)
                             }
                         }
                     }
@@ -94,45 +91,10 @@ struct CriarTarefaView: View {
                     }
                 }
                 ToolbarItem(placement: .confirmationAction) {
-                    Button {
-                        Task {
-                            isSaving = true
-                            defer { isSaving = false }
-                            
-                            guard let selectedID = selectedUserID,
-                                  let selectedUser = participants.first(where: { $0.id == selectedID }) else { return }
-                            
-                            guard let currentUser = AuthService.shared.currentUser else {
-                                throw NSError(domain: "AuthError", code: 1, userInfo: [NSLocalizedDescriptionKey: "No user loggoed in"])
-                            }
-                            let currentRef = CKRecord.Reference(recordID: currentUser.id, action: .none)
-                            
-                            let studentRef = CKRecord.Reference(recordID: selectedUser.id, action: .none)
-                            
-                            // Use TaskModel as required by PersistenceServices
-                            let task = TaskModel(
-                                title: tarefaNome,
-                                description: tarefaDescricao,
-                                student: [currentRef, studentRef],
-                                startDate: selectedDateInit,
-                                endDate: selectedDateEnd
-                            )
-                            do {
-                                try await persistenceServices.createTask(task)
-                                try? await Task.sleep(for: .seconds(1.5)) // espera CloudKit atualizar
-                                await MainActor.run {
-                                    onTarefaCriada?()
-                                    numTask += 1
-                                    dismiss()
-                                }
-                            } catch {
-                                print("Erro ao criar grupo: \(error.localizedDescription)")
-                            }
-                        }
-                    } label: {
-                        Label("Adicionar", systemImage: "checkmark")
+                    Button("Adicionar", systemImage: "checkmark") {
+                        dismiss()
                     }
-                    .disabled(tarefaNome.isEmpty || selectedUserID == nil)
+                    .disabled(tarefaNome.isEmpty || selecao == nil)
                 }
             }
         }
@@ -142,123 +104,3 @@ struct CriarTarefaView: View {
 #Preview {
     CriarTarefaView(numTask: .constant(0))
 }
-
-
-
-//import SwiftUI
-//import CloudKit
-//
-//struct CriarTarefaView: View {
-//    @State private var selectedItem: Int = 0
-//    @State private var selectedDateInit = Date()
-//    @State private var selectedDateEnd = Date()
-//    @State private var tarefaNome: String = ""
-//    @State private var tarefaDescricao: String = ""
-//    @State private var moedas: Int = 5
-//    @State private var selecao: UUID?
-//    @Environment(\.dismiss) var dismiss
-//    @Binding var numTask: Int
-//    @EnvironmentObject var persistenceServices: PersistenceServices
-//    @State private var isSaving = false
-//    var onTarefaCriada: (() -> Void)?
-//    @State private var participants: [UserModel] = []
-//    @State private var selectedUserID: CKRecord.ID? = nil
-//    
-//    init(numTask: Binding<Int>) {
-//        self._numTask = numTask
-//    }
-//    
-//    var body: some View {
-//        NavigationStack {
-//            VStack() {
-//                Form {
-//                    Section {
-//                        TextField("Nome da Tarefa (Obrigatório)", text: $tarefaNome)
-//                        TextField("Descrição (Opcional)", text: $tarefaDescricao)
-//                    }
-//                    Section("Participantes") {
-//                        Picker("Escolha um aluno", selection: $selectedUserID) {
-//                            Text("Selecione...").tag(nil as CKRecord.ID?)
-//                            
-//                            ForEach(participants, id: \.id) { user in
-//                                Text(user.name).tag(user.id as CKRecord.ID?)
-//                            }
-//                        }
-//                    }
-//                    Section {
-//                        LabeledContent("Início") {
-//                            DatePicker("", selection: $selectedDateInit)
-//                                .datePickerStyle(.compact)
-//                        }
-//                        LabeledContent("Término") {
-//                            DatePicker("", selection: $selectedDateEnd)
-//                                .datePickerStyle(.compact)
-//                        }
-//                    }
-//                }
-//            }
-//            .navigationBarTitleDisplayMode(.inline)
-//            .navigationTitle("Adicionar Tarefa")
-//            .toolbar {
-//                // If you want a cancel button, uncomment:
-//                 ToolbarItem(placement: .cancellationAction) {
-//                     Button {
-//                         dismiss()
-//                     } label: {
-//                         Label("Cancelar", systemImage: "xmark")
-//                     }
-//                 }
-//                ToolbarItem(placement: .confirmationAction) {
-//                    Button {
-//                        Task {
-//                            isSaving = true
-//                            defer { isSaving = false }
-//                            
-//                            guard let selectedID = selectedUserID,
-//                                  let selectedUser = participants.first(where: { $0.id == selectedID }) else { return }
-//                            
-//                            let studentRef = CKRecord.Reference(recordID: selectedUser.id, action: .none)
-//                            
-//                            // Use TaskModel as required by PersistenceServices
-//                            let task = TaskModel(
-//                                title: tarefaNome,
-//                                description: tarefaDescricao,
-//                                student: studentRef,
-//                                startDate: selectedDateInit,
-//                                endDate: selectedDateEnd
-//                            )
-//                            do {
-//                                try await persistenceServices.createTask(task)
-//                                try? await Task.sleep(for: .seconds(1.5)) // espera CloudKit atualizar
-//                                await MainActor.run {
-//                                    onTarefaCriada?()
-//                                    numTask += 1
-//                                    dismiss()
-//                                }
-//                            } catch {
-//                                print("Erro ao criar grupo: \(error.localizedDescription)")
-//                            }
-//                        }
-//                    } label: {
-//                        Label("Adicionar", systemImage: "checkmark")
-//                    }
-//                    .disabled(tarefaNome.isEmpty || selectedUserID == nil || isSaving)
-//                }
-//            }
-//            // If needed, load participants automatically:
-//             .task {
-//                 do {
-//                     participants = try await persistenceServices.fetchUserForTask()
-//                     print("participantes carregados: ", participants.map(\.name))
-//                 } catch {
-//                     print("Erro ao carregar participantes: \(error.localizedDescription)")
-//                 }
-//             }
-//        }
-//    }
-//}
-//
-//
-//#Preview {
-//    CriarTarefaView(numTask: .constant(0))
-//}
